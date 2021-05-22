5da606c1b789cb44207124a651587819740d87d2
Alberto Bordino <laburd@Albertos-Air.homenet.telecomitalia.it>
2021-05-22 21:18:43 +0200

    main.py update

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from tree import DecisionTree
 from forest import RandomForest
 
-tennis = pd.read_csv("data/tennis_data.csv")
-train, test = train_test_split(tennis, shuffle=False, test_size=0.33)
+tennis = pd.read_csv("tennis_data.csv")
+train, test = train_test_split(tennis, test_size=0.90)
 
-print("----> 21-10-DECISION TREE \n")
-tennis_tree = DecisionTree()
-tennis_tree.grow(train, min_leaves=21, depth=10)
-print("Full test acc: {0:.0%}".format(tennis_tree.accuracy(test)) + "\n")
-print(tennis_tree)
+
+print("----> 5-10-RANDOM FOREST \n")
+tennis_forest = RandomForest()
+tennis_forest.grow(train, min_leaves=5, depth=10, post_pruning="rep")
+print("Full test acc: {0:.0%}".format(tennis_forest.accuracy(test)) + "\n")
+
 
